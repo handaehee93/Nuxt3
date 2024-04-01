f96ea31d148ebde596fb9c57d4635a1d4fe6826f
한대희 <hdh8990@futurenuri.com>
2024-04-01 13:55:21 +0900

    layouts 디렉토리를 통한 레이아웃 페이지 활용

--- app.vue
+++ app.vue
@@ -1,23 +1,7 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-header elevated class="bg-dark text-white">
-      <q-toolbar>
-        <q-toolbar-title>Vue Master Course</q-toolbar-title>
-        <q-btn stretch flat label="Home 페이지로 이동" to="/" />
-        <q-separator dark vertical />
-        <q-btn stretch flat label="About 페이지로 이동" to="/about" />
-      </q-toolbar>
-    </q-header>
-    <q-page-container :style="pageContainerStyle">
-      <!-- NuxtPage라는 특수 컴포넌트를 사용하면 해당 부분에 url에 매핑되는 page 컴포넌트가 렌더링 된다. -->
-      <NuxtPage />
-    </q-page-container>
-  </q-layout>
+  <!-- NuxtLayout 이라는 컴포넌트를 사용해서 layouts 폴더 아래 레이아웃 파일을 불러오고, 레이아웃 파일의 slot 부분에 아래 NuxtPage를 렌더링 -->
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-<script setup lang="ts">
-// computed라는 것도 vue에서 제공하는 api인데 별다른 import 없이 사용할 수 있다.
-const pageContainerStyle = computed(() => ({
-  maxWidth: '1080px',
-  margin: '0 auto',
-}));
-</script>
+<script setup lang="ts"></script>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title>Vue Master Course</q-toolbar-title>
+        <q-btn stretch flat label="Home 페이지로 이동" to="/" />
+        <q-separator dark vertical />
+        <q-btn stretch flat label="About 페이지로 이동" to="/about" />
+      </q-toolbar>
+    </q-header>
+    <q-page-container :style="pageContainerStyle">
+      <!-- NuxtPage라는 특수 컴포넌트를 사용하면 해당 부분에 url에 매핑되는 page 컴포넌트가 렌더링 된다. -->
+      <slot></slot>
+    </q-page-container>
+  </q-layout>
+</template>
+<script setup lang="ts">
+// computed라는 것도 vue에서 제공하는 api인데 별다른 import 없이 사용할 수 있다.
+const pageContainerStyle = computed(() => ({
+  maxWidth: '1080px',
+  margin: '0 auto',
+}));
+</script>
